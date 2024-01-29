8a2a39cb3e40138e5ce9d2897d0b304dd6b5d119
peyton2919 <fz2919@tom.com>
2024-01-29 22:25:48 +0800

    update init

---
+++ plum-mall-ware/src/test/java/cn/peyton/plum/ware/WareApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.peyton.plum.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
